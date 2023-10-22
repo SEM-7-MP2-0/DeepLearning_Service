7dc7025c973f5d43a144ed5f1802551b4c135956
SalmanAd01 <salmanadhikari415@gmail.com>
2023-10-22 21:59:08 +0530

    fix cd

--- routes/index.py
+++ routes/index.py
@@ -5,4 +5,4 @@ index = Blueprint(name="index", import_name=__name__)
 
 @index.route("", methods=["GET"])
 def index_handler():
-    return "Hello Welcome to Face Detection API"
+    return "Hello Welcome to Face Detection API!!"
